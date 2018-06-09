86781640dcaf52ee7c61ea5283ef5ea0ed3a5ee7
Dikan Chen <dikanchen@dikan-chen.attlocal.net>
2018-06-09 11:17:14 -0700

    Initial Commit

---
+++ Practice/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Practice
+//
+//  Created by Dikan Chen on 6/9/18.
+//  Copyright © 2018 Dikan Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Practice/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Practice
+//
+//  Created by Dikan Chen on 6/9/18.
+//  Copyright © 2018 Dikan Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
